1e35c85c329b45e56a0508bd7a44c2ca23a3e22e
Andy <cherniakhovych.andy@gmail.com>
2024-12-26 13:48:39 +0200

    set up server, add constants

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,12 +1,20 @@
 const express = require("express");
+const cors = require("cors");
+const bodyParser = require("body-parser");
+
+require("dotenv").config();
+
+const videoRouter = require("./routes/videoRouter")
+
 const app = express();
 const PORT = 9000;
 
+app.use(cors({ origin: "*" }));
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
 app.use(express.json());
 
-app.get("/", (req, res) => {
-  res.send("Hello, Express!");
-});
+app.use("/api/video", videoRouter);
 
 app.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`);
---
+++ backend/src/utils/constants.js
@@ -0,0 +1,5 @@
+const VIDEO_BASE_URL = "https://www.googleapis.com/youtube/v3";
+
+module.exports = {
+  VIDEO_BASE_URL,
+};
